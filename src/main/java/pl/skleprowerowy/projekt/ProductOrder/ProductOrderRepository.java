5d8058974a297e41614a2f5c914d8d49daa81093
pmiluu <pmiluszkiewicz@gmail.com>
2021-06-11 22:40:50 +0200

    dodalem productOrder repo

---
+++ src/main/java/pl/skleprowerowy/projekt/ProductOrder/ProductOrderRepository.java
@@ -0,0 +1,6 @@
+package pl.skleprowerowy.projekt.ProductOrder;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductOrderRepository extends CrudRepository<ProductOrder,Long> {
+}
